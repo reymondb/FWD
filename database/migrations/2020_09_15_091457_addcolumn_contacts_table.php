4c449d6f9b9185a8efa5140f8fbe973526be83c6
reymondb <reymondb@codev.com>
2020-09-15 17:58:55 +0800

    fixes

--- database/migrations/2020_09_15_091457_addcolumn_contacts_table.php
+++ database/migrations/2020_09_15_091457_addcolumn_contacts_table.php
@@ -41,11 +41,5 @@ class AddcolumnContactsTable extends Migration
                 $table->dropColumn('ListID');
             });
         }
-        if (Schema::hasTable('new_leads')) {
-            Schema::table('new_leads', function (Blueprint $table) {
-                $table->dropColumn('PhoneCode');
-                $table->dropColumn('ListID');
-            });
-        }
     }
 }
