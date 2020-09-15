bea4fbd73c27fa8ceaad4f862574787a24647e68
reymondb <reymondb@codev.com>
2020-09-15 15:08:17 +0800

    test

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -22,6 +22,7 @@ class LeadsController extends Controller
         $contacts = Contact::select('contacts.*','campaign.CampaignName')->leftjoin("campaign_use",'campaign_use.ContactID','contacts.id')
         ->leftjoin("campaign",'campaign.id','campaign_use.CampaignID')
         ->groupBy('contacts.id')
+        ->limit(1000)
         ->get();
         //dd(DB::getQueryLog()); // Show results of log
         return view('dashboard/leads')->with('contacts',$contacts);
