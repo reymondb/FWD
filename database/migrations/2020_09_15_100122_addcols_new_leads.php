4b9daf85eb0120848577fcea5a14db08ccb9e1a2
reymondb <reymondb@codev.com>
2020-09-15 18:02:53 +0800

    fixes

---
+++ database/migrations/2020_09_15_100122_addcols_new_leads.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddcolsNewLeads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('contacts')) {
+            Schema::table('contacts', function (Blueprint $table) {
+                $table->string('PhoneCode')->after('LandlineNum')->nullable();
+                $table->string('ListID')->after('PhoneCode')->nullable(); 
+            });
+        }
+        if (Schema::hasTable('new_leads')) {
+            Schema::table('new_leads', function (Blueprint $table) {
+                $table->string('PhoneCode')->after('LandlineNum')->nullable();
+                $table->string('ListID')->after('PhoneCode')->nullable(); 
+            });
+        }
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+        if (Schema::hasTable('contacts')) {
+            Schema::table('contacts', function (Blueprint $table) {
+                $table->dropColumn('PhoneCode');
+                $table->dropColumn('ListID');
+            });
+        }
+        if (Schema::hasTable('new_leads')) {
+            Schema::table('new_leads', function (Blueprint $table) {
+                $table->dropColumn('PhoneCode');
+                $table->dropColumn('ListID');
+            });
+        }
+    }
+}
