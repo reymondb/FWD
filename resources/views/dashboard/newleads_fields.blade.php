f6ce0e22ed2a3bf682da751197ddec592560dcf7
reymondb <reymondb@codev.com>
2020-09-22 16:45:14 +0800

    add loading on washing

--- resources/views/dashboard/newleads_fields.blade.php
+++ resources/views/dashboard/newleads_fields.blade.php
@@ -79,7 +79,7 @@
             i = 1;
             var elem = document.getElementById("myBar");
             var width = 10;
-            var id = setInterval(frame, 200);
+            var id = setInterval(frame, 100);
             function frame() {
             if (width >= totalrows) {
                 clearInterval(id);
