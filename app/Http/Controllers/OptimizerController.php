25db4d89dc4aa4d079a1bbf79eed33e19378f6d0
reymondb <reymondb@codev.com>
2020-10-19 18:18:33 +0800

    check on live

--- app/Http/Controllers/OptimizerController.php
+++ app/Http/Controllers/OptimizerController.php
@@ -47,7 +47,7 @@ class OptimizerController extends Controller
         /*$leads=CampaignUse::select(DB::raw('CONCAT(campaign.CampaignName,"(",count(campaign_use.id),")") as CampaignName'),DB::raw('count(campaign_use.id) as total'))
         ->leftjoin('campaign','campaign_use.CampaignID','campaign.id')->groupby('campaign_use.CampaignID')->get();*/
         $leads=Contact::select(DB::raw('CONCAT(campaign.CampaignName,"(",count(contacts.id),")") as CampaignName'),DB::raw('count(contacts.id) as total'))
-        ->leftjoin('campaign','contacts.campaign_id','campaign.id')->groupby('campaign_use.CampaignID')->get();
+        ->leftjoin('campaign','contacts.campaign_id','campaign.id')->groupby('contacts.campaign_id')->get();
 
         Charts::where('chart_type',1)->delete();
         foreach($leads as $lead){
