46e8daa4b76c4972c3414cfb06e1f4cd41c22380
reymondb <reymondb@codev.com>
2020-10-20 22:12:41 +0800

    test

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -72,7 +72,6 @@ class ReportsController extends Controller
         else{
 
         }
-        dd(json_encode($data));
         return view('dashboard/reporting')->with('data',json_encode($data));
         
     }
