2bd7eb98223152ef38301250822bb0f43350f19e
reymondb <reymondb@codev.com>
2020-10-21 20:15:18 +0800

    add lead statistics

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -32,30 +32,35 @@
 @stop
 
 @section('js')
-        <script>
-             $.ajaxSetup({
-                headers: {
-                    'X-CSRF-Token': $('input[name=_token]').val()
-                }
-            });
-            $(document).ready(function(){
-                $("#campaign").on("change",function(){
-                    $.ajax({
-                        url: "/getleadlists?campaignid="+$("#campaign").val(),
-                        type: 'GET',
-                        contentType: false, // The content type used when sending data to the server.
-                        cache: false, // To unable request pages to be cached
-                        processData: false,
-                        success: function (data) {
-                            console.log(data);
-                            $('#list_id').empty().append('<option selected="selected" value="">Select List ID</option>');
-                            $.each(data, function(k, v) {
-                                $('#list_id').append('<option selected="selected" value="'+v.list_id+'">'+v.list_id+'</option>');
-                            });
-                                    
-                        }
-                    });
+
+    <style>
+        .filter_inputs{
+            font-size:14px;
+        }
+    </style>
+    <script>
+            $.ajaxSetup({
+            headers: {
+                'X-CSRF-Token': $('input[name=_token]').val()
+            }
+        });
+        $(document).ready(function(){
+            $("#campaign").on("change",function(){
+                $.ajax({
+                    url: "/getleadlists?campaignid="+$("#campaign").val(),
+                    type: 'GET',
+                    contentType: false, // The content type used when sending data to the server.
+                    cache: false, // To unable request pages to be cached
+                    processData: false,
+                    success: function (data) {
+                        $('#list_id').empty().append('<option selected="selected" value="">Select List ID</option>');
+                        $.each(data, function(k, v) {
+                            $('#list_id').append('<option value="'+v.list_id+'">'+v.list_id+'</option>');
+                        });
+                                
+                    }
                 });
             });
-        </script>
+        });
+    </script>
 @stop
