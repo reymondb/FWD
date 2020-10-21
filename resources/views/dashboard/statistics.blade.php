a70727c343b7e105ba9b045260285fc3e3dac527
reymondb <reymondb@codev.com>
2020-10-22 07:47:28 +0800

    add lead statistics

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -174,7 +174,31 @@
                             'excelHtml5',
                             'csvHtml5',
                             'pdfHtml5'
-                        ]
+                        ],
+                        "footerCallback": function ( row, data, start, end, display ) {
+                                    var api = this.api(), data;
+                        
+                                    // Remove the formatting to get integer data for summation
+                                    var intVal = function ( i ) {
+                                        return typeof i === 'string' ?
+                                            i.replace(/[\$,]/g, '')*1 :
+                                            typeof i === 'number' ?
+                                                i : 0;
+                                    };
+                        
+                                    // Total over all pages 1
+                                    overall = api
+                                        .column( 2 )
+                                        .data()
+                                        .reduce( function (a, b) {
+                                            return intVal(a) + intVal(b);
+                                        }, 0 );
+                                        
+                                    // Update footer
+                                    $( api.column( 1 ).footer() ).html("Total:");
+                                    
+                                    $( api.column( 2 ).footer() ).html('$'+overall);
+                                }
                     } );
                 }
             });
@@ -217,6 +241,10 @@
                 }
             });
         }
+
+        $(document).ready(function() {
+    
+} );
     </script>
     
     
