bcc6c0b5194fbc4d13e1c16f8954b4bc3acf12e3
reymondb <reymondb@codev.com>
2020-10-22 16:59:50 +0800

    export please

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -352,7 +352,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <!--<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>!-->
-    <script src="{{ URL::asset('js/buttons.html5.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/buttons.html5.min.js') }}"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
     
 @stop
