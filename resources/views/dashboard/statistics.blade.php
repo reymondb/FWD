fbe43619f197dd62455e917fd383fb148ef88c8a
reymondb <reymondb@codev.com>
2020-10-22 16:37:40 +0800

    add lead additionals

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -26,23 +26,23 @@
                                 <thead>
                                     <tr><th colspan='16'>From vicidial_list (Overall Lead Total: <span id="over_all">0</span> )</th></tr>
                                     
-                                    <tr><th colspan=2 class="report_th">Original Batch Date</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">Batch ID</th><th colspan=2 class="report_th" id="batch_id"></th></tr> 
-                                    <tr><th colspan=2 class="report_th">LEAD COST (AUD)</th><th colspan=2 class="report_th" id="lead_cost_report"></th></tr>
-                                    <tr><th colspan=2 class="report_th">TOTAL NUMBERS IN THE FILE</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">Lead Batch Dialer Cycle</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">TOTAL QUALIFIED LEADS (QL)</th><th colspan=2 class="report_th" id="replace"></th></tr>
+                                    <tr><th colspan=2 class="report_th">Original Batch Date</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">Batch ID</th><th colspan=2 class="report_th" id="batch_id"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr> 
+                                    <tr><th colspan=2 class="report_th">LEAD COST (AUD)</th><th colspan=2 class="report_th" id="lead_cost_report"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">TOTAL NUMBERS IN THE FILE</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">Lead Batch Dialer Cycle</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">TOTAL QUALIFIED LEADS (QL)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
 
-                                    <tr><th colspan=2 class="report_th">COST / QL (CPQL)</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">COST / LEAD (CPL)</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">Penetration (Human Ans)</th><th colspan=2 class="report_th" id="human_answered"></th></tr>
-                                    <tr><th colspan=2 class="report_th">Penetration Rate</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">COST / Contactable LEAD (CPCL)</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">FX</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">COST / QL (PHP)</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">COST / LEAD (PHP)</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">COST / Contactable LEAD (PHP)</th><th colspan=2 class="report_th" id="replace"></th></tr>
-                                    <tr><th colspan=2 class="report_th">TOTAL DIALS</th><th colspan=2 class="report_th" id="replace"></th></tr>
+                                    <tr><th colspan=2 class="report_th">COST / QL (CPQL)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">COST / LEAD (CPL)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">Penetration (Human Ans)</th><th colspan=2 class="report_th" id="human_answered"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">Penetration Rate</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">COST / Contactable LEAD (CPCL)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">FX</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">COST / QL (PHP)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">COST / LEAD (PHP)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">COST / Contactable LEAD (PHP)</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                                    <tr><th colspan=2 class="report_th">TOTAL DIALS</th><th colspan=2 class="report_th" id="replace"></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
                                     
                                     <tr>
                                         <th colspan=4></th>
@@ -209,82 +209,7 @@
                         dom: 'Bfrtip',
                         buttons: [
                             { extend: 'copyHtml5', header:true, footer: true },
-                            { extend: 'excelHtml5', header:true, footer: true,
-                            customize: function (xlsx) {
-                                //Apply styles, Center alignment of text and making it bold.
-                                var sSh = xlsx.xl['styles.xml'];
-                                var lastXfIndex = $('cellXfs xf', sSh).length - 1;
-
-                                var n1 = '<numFmt formatCode="##0.0000%" numFmtId="300"/>';
-                                var s2 = '<xf numFmtId="0" fontId="2" fillId="0" borderId="0" applyFont="1" applyFill="0" applyBorder="0" xfId="0" applyAlignment="1">' +
-                                        '<alignment horizontal="center"/></xf>';
-
-                                sSh.childNodes[0].childNodes[0].innerHTML += n1;
-                                sSh.childNodes[0].childNodes[5].innerHTML += s2;
-
-                                var greyBoldCentered = lastXfIndex + 1;
-
-                                //Merge cells as per the table's colspan
-                                var sheet = xlsx.xl.worksheets['sheet1.xml'];
-                                var dt = $('#tblReport').DataTable();
-                                var frColSpan = $(dt.table().header()).find('th:nth-child(1)').prop('colspan');
-                                var srColSpan = $(dt.table().header()).find('th:nth-child(2)').prop('colspan');
-                                var columnToStart = 2;
-
-                                var mergeCells = $('mergeCells', sheet);
-                                mergeCells[0].appendChild(_createNode(sheet, 'mergeCell', {
-                                    attr: {
-                                        ref: 'A1:' + toColumnName(frColSpan) + '1'
-                                    }
-                                }));
-
-                                mergeCells.attr('count', mergeCells.attr('count') + 1);
-
-                                var columnToStart = 2;
-
-                                while (columnToStart <= frColSpan) {
-                                    mergeCells[0].appendChild(_createNode(sheet, 'mergeCell', {
-                                        attr: {
-                                            ref: toColumnName(columnToStart) + '2:' + toColumnName((columnToStart - 1) + srColSpan) + '2'
-                                        }
-                                    }));
-                                    columnToStart = columnToStart + srColSpan;
-                                    mergeCells.attr('count', mergeCells.attr('count') + 1);
-                                }
-
-                                //Text alignment to center and apply bold
-                                $('row:nth-child(1) c:nth-child(1)', sheet).attr('s', greyBoldCentered);
-                                for (i = 0; i < frColSpan; i++) {
-                                    $('row:nth-child(2) c:nth-child(' + i + ')', sheet).attr('s', greyBoldCentered);
-                                }
-
-                                function _createNode(doc, nodeName, opts) {
-                                    var tempNode = doc.createElement(nodeName);
-                                    if (opts) {
-                                        if (opts.attr) {
-                                            $(tempNode).attr(opts.attr);
-                                        }
-                                        if (opts.children) {
-                                            $.each(opts.children, function (key, value) {
-                                                tempNode.appendChild(value);
-                                            });
-                                        }
-                                        if (opts.text !== null && opts.text !== undefined) {
-                                            tempNode.appendChild(doc.createTextNode(opts.text));
-                                        }
-                                    }
-                                    return tempNode;
-                                }
-
-                                //Function to fetch the cell name
-                                function toColumnName(num) {
-                                    for (var ret = '', a = 1, b = 26; (num -= a) >= 0; a = b, b *= 26) {
-                                        ret = String.fromCharCode(parseInt((num % b) / a) + 65) + ret;
-                                    }
-                                    return ret;
-                                }
-                        
-                            }},
+                            { extend: 'excelHtml5', header:true, footer: true },
                             { extend: 'csvHtml5', header:true, footer: true },
                             { extend: 'pdfHtml5', header:true, footer: true }
                         ],
