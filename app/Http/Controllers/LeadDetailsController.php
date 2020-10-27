39c363d94c37d4e5f8e084224bd8c96e5008f670
reymondb <reymondb@codev.com>
2020-10-27 14:31:08 +0800

    test

--- app/Http/Controllers/LeadDetailsController.php
+++ app/Http/Controllers/LeadDetailsController.php
@@ -69,8 +69,8 @@ class LeadDetailsController extends Controller
                 $dataz = DB::connection('mysql_external')
                     ->table('vicidial_list')
                     ->select('vicidial_list.phone_number','vicidial_list.lead_id','vicidial_statuses.status_name','vicidial_log.call_date','vicidial_list.entry_date','vicidial_log.campaign_id')
-                    ->leftjoin('vicidial_statuses','vicidial_statuses.status','vicidial_list.status')
                     ->leftjoin('vicidial_log','vicidial_log.phone_number','vicidial_list.phone_number')
+                    ->leftjoin('vicidial_statuses','vicidial_statuses.status','vicidial_log.status')
                     ->where('vicidial_list.phone_number',"$num")
                     ->get();
                 //    dd(DB::getQueryLog());
